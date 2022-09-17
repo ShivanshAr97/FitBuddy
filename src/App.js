52234cd9112867f16dd54ee8ef8fc0d304b1e470
ShivanshAr97 <96040322+ShivanshAr97@users.noreply.github.com>
2022-09-17 18:59:59 +0530

    Starter template

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+    <h1 className='bg-red-500 text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi eos aliquid doloribus, perspiciatis accusamus aspernatur! Sunt ducimus numquam esse ipsum placeat, nemo perferendis ullam rerum rem doloribus quaerat assumenda maiores!</h1>
+    </>
+  );
+}
+
+export default App;
